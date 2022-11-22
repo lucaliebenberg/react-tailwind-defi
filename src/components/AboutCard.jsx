63a72790d1e66548cd53aa86a03f696fa54b9db6
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2022-11-22 08:42:36 +0200

    About + AboutCard component add

---
+++ src/components/AboutCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AiFillPropertySafety } from "react-icons/ai";
+
+const AboutCard = (props) => {
+  return (
+    <div className="flex flex-col border text-left rounded-2xl py-12 px-8">
+      <div>
+        <div className="bg-[var(--primary-blue)] inline-flex p-2 rounded-full">
+          {props.icon}
+        </div>
+        <h3 className="text-xl font-bold py-4">{props.heading}</h3>
+        <p>{props.text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutCard;
