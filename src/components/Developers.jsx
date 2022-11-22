a61747994f6c70c05f7dbf23b40f4d722f47f36b
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2022-11-22 09:13:33 +0200

    Developers minor change + Footer component add

--- src/components/Developers.jsx
+++ src/components/Developers.jsx
@@ -4,8 +4,8 @@ import terminal from "../assets/terminal.png";
 
 const Developers = () => {
   return (
-    <div className="w-full text-white bg-black">
-      <div className="max-w-[1240px] px-4 py-16 md:flex">
+    <div className="w-full text-white bg-black pl-[200px] pt-[50px]">
+      <div className="max-w-[1240px] mx-auto px-4 py-16 md:flex">
         <div>
           <h1>Superpowers for DeFi developers.</h1>
           <p>
