5b9ff03ec2765ec48bfb69f37e8b81c6d3a9313e
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-02-12 09:54:16 +0200

    Initial project commit - asstes + navbar and hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,37 @@
 import React from "react";
 
-import heroVid from "../assets/video.mp4";
+// imports
+import heroImage from "../assets/heroProductImage.png";
 
 const Hero = () => {
   return (
-    <div className="w-full h-[90vh] top-[90px]">
-      <video
-        className="object-cover h-full w-full absolute -z-10"
-        src={heroVid}
-        autoPlay
-        loop
-        muted
-      />
-      <div className="w-full h-[90%] flex flex-col justify-center items-center text-white px-4 text-center">
-        <h1>Decentralized</h1>
+    <div className="w-full h-[100vh] top-[90px]">
+      <div className="w-full h-[90%] flex flex-col justify-center items-center text-[var(--primary-blue)] px-4 text-center">
+        <h1>Stupidly Simple </h1>
         <h1 className="py-2">
-          <span className="text-[var(--primary-blue)]">Trading</span> Protocol
+          <span className="text-[var(--primary-blue)]">Accounting</span>{" "}
+          Software
         </h1>
-        <p className="text-xl py-4">
-          Guaranteed liquidity trading for millions of users and top Ethereum
-          applications
+        <div className="my-4">
+          <img
+            src={heroImage}
+            alt="Onesheet Product"
+            width={500}
+            height={500}
+          />
+        </div>
+        <p className="text-xl py-[3rem] max-w-[55rem]">
+          Onesheet is an intuitive online accounting software that provides
+          users with an easy way to track financial information, manage
+          expenses, and generate detailed reports with just a few clicks.
         </p>
         <div className="py-4">
-          <button className="m-2">Use DeFi</button>
-          <button className="m-2">FAQ</button>
+          {/* <button className="m-2">Start free trial</button> */}
+          {/* <button className="m-2">Learn More</button> */}
         </div>
-      </div>
-      <div className="">
-        <p className="text-center text-white text-2xl">
-          Total Volume Secured: $42,104,783,662,47
-        </p>
+        <div className="d-flex w-full h-full">
+          <div className="justify-center items-center text-white"></div>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,27 +8,27 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full h-[90px] bg-black">
+    <div className="w-full h-[90px] bg-white">
       <div className="max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full">
         <div>
-          <h1 className="text-[var(--primary-blue)]">DEFI</h1>
+          <h1 className="text-[var(--primary-blue)]">ONESHEET</h1>
         </div>
         <div className="hidden md:flex">
-          <ul className="flex text-white items-center">
-            <li>Platform</li>
-            <li>Developers</li>
-            <li>Community</li>
+          <ul className="flex text-[var(--primary-blue)] items-center">
             <li>About</li>
-            <button className="ml-4">Use DeFi</button>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+            <button className="ml-4">Start free trial</button>
           </ul>
         </div>
 
         {/* Hamburger Menu */}
         <div className="block md:hidden" onClick={handleNav}>
           {nav ? (
-            <AiOutlineClose size={30} className="text-white" />
+            <AiOutlineClose size={30} className="text-[var(--primary-blue)]" />
           ) : (
-            <AiOutlineMenu size={30} className="text-white" />
+            <AiOutlineMenu size={30} className="text-[var(--primary-blue)]" />
           )}
         </div>
 
@@ -41,11 +41,11 @@ const Navbar = () => {
           }
         >
           <ul>
-            <li className="text-2xl">Platform</li>
-            <li className="text-2xl">Developers</li>
-            <li className="text-2xl">Community</li>
             <li className="text-2xl">About</li>
-            <button className="m-8">Use DeFi</button>
+            <li className="text-2xl">Features</li>
+            <li className="text-2xl">Pricing</li>
+            <li className="text-2xl">Contact</li>
+            <button className="m-8">Start free trial</button>
           </ul>
         </div>
       </div>
